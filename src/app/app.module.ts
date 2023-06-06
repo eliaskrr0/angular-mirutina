859ee4be5fa3490d526464b72b7f58171a4461c9
Elias <ilyasseessadak@gmail.com>
2023-06-06 21:50:35 +0200

    creando componentes de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooComponent } from './components/foo/foo.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
